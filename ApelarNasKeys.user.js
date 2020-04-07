323f1c7c438ff7f57e4e49c0fd22daa1ef5538db
cassiomaciell <32285757+cassiomaciell@users.noreply.github.com>
2020-04-07 00:44:25 -0300

    Update ApelarNasKeys.user.js

--- ApelarNasKeys.user.js
+++ ApelarNasKeys.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Apelar nas keys
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0
-// @description  hm
+// @version      1.0.1
+// @description  try to take over the world!
 // @author       CassioMaciel
 // @match        https://www.twitch.tv/*
 // @grant        none
@@ -13,42 +13,44 @@
 (function () {
     'use strict';
 
-    window.apelarKey = false
+    document.onload = () => {
+        window.apelarKey = false
 
-    const copyToClipboard = str => {
-        const el = document.createElement('textarea');
-        el.value = str;
-        document.body.appendChild(el);
-        el.select();
-        document.execCommand('copy');
-        document.body.removeChild(el);
-    };
-    document.addEventListener('click', e => {
-        if(!window.apelarKey) return
-        if (e.target.classList.contains('chat-line__message')) {
-            copyToClipboard(e.target.querySelector('.text-fragment').innerText)
-        } else if (e.target.classList.contains('text-fragment')) {
-            copyToClipboard(e.target.querySelector('.text-fragment').innerText)
+        const copyToClipboard = str => {
+            const el = document.createElement('textarea');
+            el.value = str;
+            document.body.appendChild(el);
+            el.select();
+            document.execCommand('copy');
+            document.body.removeChild(el);
+        };
+        document.addEventListener('click', e => {
+            if (!window.apelarKey) return
+            if (e.target.classList.contains('chat-line__message')) {
+                copyToClipboard(e.target.querySelector('.text-fragment').innerText)
+            } else if (e.target.classList.contains('text-fragment')) {
+                copyToClipboard(e.target.querySelector('.text-fragment').innerText)
+            }
+        })
+
+        document.querySelector('.chat-list__list-container').addEventListener('DOMNodeInserted', e => {
+            if (!e.srcElement || !e.srcElement.classList.contains('chat-line__message') || !window.apelarKey) return
+            if (!isMod(e.srcElement) && !isVip(e.srcElement) && !isChannelOwner(e.srcElement)) {
+                e.srcElement.hidden = 'true'
+            }
+        })
+
+        function isMod(e) {
+            return !!e.querySelector("img[alt='Moderador']")
         }
-    })
 
-    document.querySelector('.chat-list__list-container').addEventListener('DOMNodeInserted', e => {
-        if (!e.srcElement || !e.srcElement.classList.contains('chat-line__message') || !window.apelarKey) return
-        if (!isMod(e.srcElement) && !isVip(e.srcElement) && !isChannelOwner(e.srcElement)) {
-            e.srcElement.hidden = 'true'
+        function isChannelOwner(e) {
+            return !!e.querySelector("img[alt='Emissora']")
         }
-    })
 
-    function isMod(e) {
-        return !!e.querySelector("img[alt='Moderador']")
-    }
-
-    function isChannelOwner(e) {
-        return !!e.querySelector("img[alt='Emissora']")
-    }
-
-    function isVip(e) {
-        return !!e.querySelector("img[alt='VIP']")
+        function isVip(e) {
+            return !!e.querySelector("img[alt='VIP']")
+        }
     }
 
 })();
